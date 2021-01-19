add2ba4b73fa72f7bb5ffdecb48408faf8c178ff
JohnDJake <john.d.jake@gmail.com>
2021-01-19 14:44:34 -0800

    made table names plural for readability

--- schema.sql
+++ schema.sql
@@ -6,30 +6,30 @@ CREATE DATABASE employee_tracker_db;
 USE employee_tracker_db;
 
 -- Create table of departments
-CREATE TABLE department (
+CREATE TABLE departments (
     id INT AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
 -- Create table of roles
-CREATE TABLE role (
+CREATE TABLE roles (
     id INT AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
     department_id INT NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (department_id) REFERENCES department(id)
+    FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
 -- Create table of employees
-CREATE TABLE employee (
+CREATE TABLE employees (
     id INT AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT,
     PRIMARY KEY (id),
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
