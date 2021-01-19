799fdc7358ffbb825f8ef090bc3b5ec3ca229583
JohnDJake <john.d.jake@gmail.com>
2021-01-19 14:59:18 -0800

    updated references to id keys

--- schema.sql
+++ schema.sql
@@ -9,7 +9,7 @@ USE employee_tracker_db;
 CREATE TABLE departments (
     department_id INT AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (department_id)
 );
 
 -- Create table of roles
@@ -18,8 +18,8 @@ CREATE TABLE roles (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
     department_id INT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (department_id) REFERENCES departments(id)
+    PRIMARY KEY (role_id),
+    FOREIGN KEY (department_id) REFERENCES departments(department_id)
 );
 
 -- Create table of employees
@@ -29,7 +29,7 @@ CREATE TABLE employees (
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (role_id) REFERENCES roles(id),
-    FOREIGN KEY (manager_id) REFERENCES employees(id)
+    PRIMARY KEY (employee_id),
+    FOREIGN KEY (role_id) REFERENCES roles(role_id),
+    FOREIGN KEY (manager_id) REFERENCES employees(employee_id)
 );
