a629cf252700cf52b341712985c2e2d09d883ff7
JohnDJake <john.d.jake@gmail.com>
2021-01-19 14:57:22 -0800

    changed id keys to include the name of the table

--- schema.sql
+++ schema.sql
@@ -7,14 +7,14 @@ USE employee_tracker_db;
 
 -- Create table of departments
 CREATE TABLE departments (
-    id INT AUTO_INCREMENT,
+    department_id INT AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
 -- Create table of roles
 CREATE TABLE roles (
-    id INT AUTO_INCREMENT,
+    role_id INT AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
     department_id INT NOT NULL,
@@ -24,7 +24,7 @@ CREATE TABLE roles (
 
 -- Create table of employees
 CREATE TABLE employees (
-    id INT AUTO_INCREMENT,
+    employee_id INT AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
