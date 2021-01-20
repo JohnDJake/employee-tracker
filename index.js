1f586c3b6744609c92fa475c6d53f696a4a1d576
JohnDJake <john.d.jake@gmail.com>
2021-01-19 16:17:06 -0800

    created main js file and set up db connection

---
+++ index.js
@@ -0,0 +1,15 @@
+// Configure process.env including expansion for .env variables referencing existing environment variables
+require("dotenv-expand")(require("dotenv").config());
+// Import packages
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
+
+// Create database connection, variables should be configured in .env file
+const connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST || "localhost",
+    port: process.env.MYSQL_PORT || 3306,
+    user: process.env.MYSQL_USER || "root",
+    password: process.env.MYSQL_PASSWORD || "password",
+    database: "employee_tracker_db"
+});
