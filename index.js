ae56d3a03d09e03e86c6c77600754aa7deee951f
JohnDJake <john.d.jake@gmail.com>
2021-01-20 21:26:10 -0800

    added comments

--- index.js
+++ index.js
@@ -42,9 +42,12 @@ function mainMenu() {
     }).then(({ action }) => action());
 }
 
+// Add a department
 async function addDepartment() {
     try {
+        // Generate an array of department name strings to prevent making duplicate departments
         const departments = (await connection.queryPromise("SELECT name FROM departments")).map(row => row.name);
+        // Get the new department name
         const newDepartment = inquirer.prompt({
             type: "input",
             name: "name",
@@ -52,67 +55,84 @@ async function addDepartment() {
             filter: input => input.trim(),
             validate: input => departments.includes(input) ? "That department already exists" : true
         });
+        // Add the new department to the database
         await connection.queryPromise("INSERT INTO departments SET ?", await newDepartment);
         console.log("The department was successfully added!");
     } catch (err) { console.error(err); }
     mainMenu();
 }
 
+// Add a role to a department
 async function addRole() {
     try {
         const newRole = inquirer.prompt([{
+            // Retrieve the departments from the database and have the user choose one
             type: "list",
             name: "department_id",
             message: "Choose a department to add a role to",
             choices: (await connection.queryPromise("SELECT * FROM departments")).map(dept => ({ name: dept.name, value: dept.department_id }))
         }, {
+            // Have the user set a name for the new role
             type: "input",
             name: "title",
             message: "What is the title for this new role?",
             filter: input => input.trim(),
+            // Retrieve a list of roles in the selected department and make sure the title input doesn't already exist
             validate: async function (input, answers) {
                 return (await connection.queryPromise("SELECT title FROM roles WHERE ?", answers)).map(row => row.title).includes(input) ? "That role already exists" : true;
             }
         }, {
+            // Ask the user what the salary is in this role
             type: "number",
             name: "salary",
             message: ({ title }) => `What is the salary for ${title} employees?`,
             filter: input => isNaN(input) ? "" : input,
             validate: input => input === "" || isNaN(input) ? "Please enter the salary as a number" : true
         }]);
+        // Add the new role to the database
         await connection.queryPromise("INSERT INTO roles SET ?", await newRole);
         console.log("The role was successfully added!");
     } catch (err) { console.error(err); }
     mainMenu();
 }
 
+// Add an employee
 async function addEmployee() {
     try {
         const newEmployee = inquirer.prompt([{
+            // Retrieve the departments from the database and have the user choose one
             type: "list",
             name: "department_id",
             message: "Choose a department to add an employee to",
             choices: (await connection.queryPromise("SELECT * FROM departments")).map(dept => ({ name: dept.name, value: dept.department_id }))
         }, {
+            // Retrieve the list of roles in the selected department and have the user choose one
             type: "list",
             name: "role_id",
             message: "Choose a role in that department to add an employee to",
+            // If the department doesn't have any roles, return 0 as the role_id
             choices: async function (answers) {
                 const choices = (await connection.queryPromise("SELECT role_id, title FROM roles WHERE ?", answers)).map(role => ({ name: role.title, value: role.role_id }));
                 return choices.length > 0 ? choices : [{ name: "Please add a role to this department before adding an employee.\n  Press enter to go back to the main menu.", value: 0, short: "Create a role" }];
             }
         }, {
+            // Ask for the new employee's first name
             type: "input",
             name: "first_name",
             message: "What is the new employee's first name?",
+            // Skip this question if role_id is 0 because that means the user needs to create a role first
             when: ({ role_id }) => role_id
         }, {
+            // Ask for the new employee's last name
             type: "input",
             name: "last_name",
             message: "What is the new employee's last name?",
+            // Skip this question if role_id is 0 because that means the user needs to create a role first
             when: ({ role_id }) => role_id
         }]);
+        // Only add the employee to the database if there was a role in the department to add them to
         if ((await newEmployee).role_id) {
+            // Add the new employee to the database, using an object with all of the properties of newEmployee except department_id because the remaining keys match the table columns
             await connection.queryPromise("INSERT INTO employees SET ?", (({ department_id, ...newEmployee }) => newEmployee)(await newEmployee));
             console.log("The employee was successfully added!");
         }
@@ -120,11 +140,13 @@ async function addEmployee() {
     mainMenu();
 }
 
+// View all departments
 async function viewAllDepartments() {
     try { console.table(await connection.queryPromise("SELECT * FROM departments")); } catch (err) { console.error(err); }
     mainMenu();
 }
 
+// View all roles, sorted by department
 async function viewAllRoles() {
     try {
         console.table(await connection.queryPromise(`
@@ -135,6 +157,7 @@ async function viewAllRoles() {
     mainMenu();
 }
 
+// View all employees
 async function viewAllEmployees() {
     try {
         console.table(await connection.queryPromise(`
@@ -146,6 +169,7 @@ async function viewAllEmployees() {
     mainMenu();
 }
 
+// End the database connection and quit the app by not calling mainMenu()
 function quit() {
     connection.end();
 }
